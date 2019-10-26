42ac865a550f292d1a049b21c2857bf6b6ebdb93
brian dostilio <briand787b@gmail.com>
2019-10-26 11:16:32 -0400

    initial commit

---
+++ 01_first/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <div>Hello World</div>
+    <div v-if="truthy">Conditionally Shown</div>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+
+export default {
+  name: 'App',
+  data: {
+    truthy: true
+  },
+  components: {
+    HelloWorld,
+    truthy: true
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
