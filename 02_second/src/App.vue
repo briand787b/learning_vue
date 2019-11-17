2942ef67ad85dfcc8180cb68e7b11acabe17154b
brian <bdostilio@sparkfly.com>
2019-11-16 19:25:10 -0500

    new lesson

---
+++ 02_second/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <Try/>
+    <img src="./assets/logo.png">
+    <HelloWorld/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+import Try from './components/Try'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    Try
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ 02_second/src/components/Try.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="try">
+        <p v-if="true">Try This!</p>
+        <p v-if="false">Don't Try This!</p>
+        <p>Users</p>
+        <ul>
+            <li v-for="name in usernames" v-bind:key="name">{{name}}</li>
+        </ul>
+        <button :disabled="disabled">Button!</button>
+        <p>Seconds: {{seconds}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Try',
+    data() {
+        return {
+            seconds: 0,
+            disabled: true,
+            usernames: [
+                "Brian",
+                "Tia",
+                "Pepper",
+                "Ivy"
+            ]
+        }
+    },
+    created() {
+        setInterval(() => this.seconds++, 1000); 
+    }
+}
+</script>
